d74dadd7e28c1621fec3c17fcf040fbf74dc500a
ct3pi <ct3pi@Charles-PC>
2025-03-28 13:53:31 -0400

    Tree and Node

---
+++ eclipse-workspace/DBMS/src/BSTNode.java
@@ -0,0 +1,15 @@
+
+public class BSTNode {
+	int key;
+	long filePosition;
+	BSTNode left, right;
+	
+	public BSTNode(int key, long offset) {
+		this.key = key;
+		this.filePosition = filePosition;
+		this.left = null;
+		this.right = null;
+	}
+	
+	
+}
--- eclipse-workspace/DBMS/src/BinarySearchTree.java
+++ eclipse-workspace/DBMS/src/BinarySearchTree.java
@@ -1,4 +1,31 @@
 
 public class BinarySearchTree {
-
+	private BSTNode root;
+	
+	public void insert(int key, long filePosition) {
+		root = insertRec(root, key, filePosition);
+	}
+	
+	public BSTNode insertRec(BSTNode root, int key, long filePosition) {
+		if (root == null) return new BSTNode(key, filePosition);
+		
+		if (key < root.key) {
+			root.left = insertRec(root.left, key, filePosition);
+		} else if (key > root.key) {
+			root.right = insertRec(root.right, key, filePosition);
+		}
+		
+		return root;
+	}
+	
+	public Long search(int key) {
+		return searchRec(root, key);
+	}
+	
+	public Long searchRec(BSTNode root, int key) {
+		if (root == null) return null;
+		if (key == root.key) return root.filePosition;
+		if (key < root.key) return searchRec(root.left, key);
+		return searchRec(root.right, key);
+	}
 }
